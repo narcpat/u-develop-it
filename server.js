8f925c7e3bfa7c97057852329c13760c39aa1d39
narcpat <narcissearts@gmail.com>
2022-05-22 21:16:22 -0400

    connected server.js to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 // PORT designation and the app expression
 const PORT = process.env.PORT || 3001;
@@ -7,6 +8,23 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySQL username
+    user: "root",
+    // Your MySQL password
+    password: "root_password1",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
